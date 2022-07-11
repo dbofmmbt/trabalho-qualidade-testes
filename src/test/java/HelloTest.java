d69e8f921bfbf5a89263187090260839baec91bb
Eduardo Canellas de Oliveira <eduardo.canellas@bemobi.com>
2022-07-11 08:43:04 -0300

    add support for junit5

---
+++ src/test/java/HelloTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class HelloTest {
+    @Test
+    public void test() {
+        Assertions.assertTrue(true);
+    }
+}
